920c213c376ea364fe3805737e2325e6b6f1ffd2
Chaitanya <chaitanya40>
2018-06-02 15:21:51 +0530

    add package import

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -6,7 +6,7 @@
           <input v-bind:class="{ input: true, 'is-danger': inputError}"  type="text" v-model="searchTerm" placeholder="Find a repository">
         </div>
         <div class="control">
-          <a class="button is-info" v-on:click="searchRepo">
+          <a v-bind:class="{ button: true, 'is-info': true, 'is-loading': isLoading }" v-on:click="searchRepo">
             Search
           </a>
         </div>
@@ -16,16 +16,20 @@
       <table class="table column is-offset-3 full-width">
         <thead>
           <tr>
-            <th>Repo link</th>
+            <th> Repo Name </th>
+            <th> Owner </th>
             <th> Number of Stars</th>
             <th> Number of forks</th>
             <th></th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(repo, index) in repositories" v-bind:key="index">
+          <tr v-for="(repo, index) in repositories" v-bind:key="index"  v-bind:class="{'is-selected': repo.imported}">
             <td>
-              <a :href="repo.link"> {{repo.full_name}}</a>
+              <a> {{repo.name}}</a>
+            </td>
+            <td>
+              <a> {{repo.owner}}</a>
             </td>
             <td>
               {{repo.forks_count}}
@@ -34,12 +38,12 @@
               {{repo.stars}}
             </td>
             <td>
-              <a class="button"> Import </a>
+              <a class="button" :disabled="repo.imported" :id="'import-'+ repo.id "  v-on:click="importRepo($event, repo)"> Import </a>
             </td>
           </tr>
         </tbody>
       </table>
-    </div>  
+    </div>
   </div>
 </template>
 
@@ -51,21 +55,40 @@ export default {
     return {
       searchTerm: '',
       inputError: false,
-      repositories: []
+      repositories: [],
+      isLoading: false
     }
   },
   methods: {
     searchRepo: function () {
       var vm = this
+      vm.isLoading = true
       if (this.searchTerm.length === 0) {
         this.inputError = true
       } else {
         axios.get('http://localhost:3000/search?q=' + vm.searchTerm)
           .then(response => {
-            vm.repositories = response.data.repos            
+            vm.repositories = response.data.repos
+            vm.isLoading = false
             console.log(response.data.repos)
           })
       }
+    },
+    importRepo: function (e, repo) {
+      var elem = document.getElementById('import-'+repo.id)
+      elem.text = 'Importing....'
+      axios.post('http://localhost:3000/import', {repo_name: repo.name, owner: repo.owner})
+        .then(response => {
+          if(response.data.status === '200'){
+            elem.text = 'Imported'
+            elem.parentNode.parentNode.className+=' is-selected'
+            elem.disabled = true
+          }
+          else{
+            elem.text = 'Import failed'
+            console.log(response.data)
+          }
+        })
     }
   }
 
--- src/components/TopPackages.vue
+++ src/components/TopPackages.vue
@@ -1,15 +1,50 @@
 <template>
-  <h1>Here are a list of top packages used</h1>
+  <div class="container is-fluid">
+    <div class="columns">
+      <table class="table column is-offset-5 full-width">
+        <thead>
+          <tr>
+            <th>
+              Package
+            </th>
+            <th>
+              Count
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(pkg, index) in packages" v-bind:key="index">
+            <td>{{pkg.name}}</td>
+            <td>{{pkg.count}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'TopPackages',
   data () {
     return {
       packages: []
     }
+  },
+  methods: {
+    
+  },
+
+  created: function () {
+    var vm = this
+    axios.get('http://localhost:3000/packages')
+      .then(response => {
+        vm.packages = response.data.packages
+      })
   }
+
 }
 </script>
 
